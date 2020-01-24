55b91ef84a96c02ebdedef9db5a3e655a49e5570
Вероника Шилова <veronikasilova@MacBook-Pro-Veronika.local>
2020-01-24 11:22:55 +0100

    STL includes

---
+++ STL.h
@@ -0,0 +1,14 @@
+//
+//  STL.h
+
+#pragma once
+
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <algorithm>
+#include <vector>
+#include <function>
+#include "assert.h"
+
+using namespace std;
