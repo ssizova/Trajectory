78fc8ed1ffcc57abe50f1cb946d96fd99e81057b
NikitaMatckevich <57538710+NikitaMatckevich@users.noreply.github.com>
2020-02-07 10:43:33 +0100

    Added new STL including header

---
+++ includes.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <cmath>
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <list>
+#include <forward_list>
+#include <unordered_map>
+#include <algorithm>
+#include <limits>
+#include "assert.h"
+
+using id_t = unsigned int;
+
+extern const double inf;
