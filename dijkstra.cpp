dfcd8a746d28c9ffe08febf8adf27fedc967490f
NikitaMatckevich <57538710+NikitaMatckevich@users.noreply.github.com>
2020-02-15 23:15:57 +0100

    changed tabs and const references

--- dijkstra.cpp
+++ dijkstra.cpp
@@ -1,24 +1,22 @@
 #include "dijkstra.h"
 
-list<id_t> dijkstra(const id_t s, const id_t t, no_graph const& g) {
+list<id_t> dijkstra(const id_t s, const id_t t, const no_graph& g) {
   auto n = g.num_vertices();
   vector<double> path_l(n, inf);
-  path_l.shrink_to_fit();
   path_l[s] = 0.;
-  vector<id_t> path_p;
-  path_p.reserve(n);
+  vector<id_t> path_p(n);
   forward_list<id_t> unvisited;
   for (id_t i = 0; i < n; i++) {
     unvisited.emplace_front(i);
   } // list of unvisited vertices
   id_t curr = s;
   while (curr != t) {
+    auto it_before_min = unvisited.before_begin();
     auto it_min  = unvisited.begin();
     auto it_last = unvisited.end();
     auto it_prev = unvisited.begin();
     auto it_curr = it_prev;
     ++it_curr;
-    auto it_before_min = unvisited.before_begin();
     while (it_curr != it_last) {
       if (path_l[*it_curr] < path_l[*it_min]) {
         it_min        = it_curr;
