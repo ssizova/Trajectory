3b8eb1af29c88176016a0115bedfb25206a556e3
NikitaMatckevich <57538710+NikitaMatckevich@users.noreply.github.com>
2020-02-25 19:44:56 +0100

    reading of map and SFINAE tricks
    Reading of the map is now a part of config file.
    User now can choose any IO files on his system to be used by the program.
    This code uses std::filesystem library so should work fine on any OS.
    Code is tolerant to the changes of containers used to store map elements. SFINAE complile-time tricks are implemented to ensure correct container behavior.

--- config.cpp
+++ config.cpp
@@ -1,104 +1,181 @@
 #include "config.h"
 
-namespace config {
-	path& input_file(value_type* arg = nullptr) {
-		static path res{ arg };
-		return res;
-	}
-	path& output_file(value_type* arg = nullptr) {
-		static path res{ arg };
-		return res;
-	}
-	template<class Container>
-	void output(const Container& c) {
-		filesystem::copy_file(input_file(), output_file(), filesystem::copy_options::overwrite_existing);
-		ofstream out(output_file(), ios::app);
-		out << endl;
-		out << "[$OptPath]" << endl;
-		for (const auto& elem : c) {
-			out << elem << endl;
-		}
-		out.close();
-	}
-
-  namespace {
-    bool section_find(ifstream& in, const string& sectname, const path& filename) {
-      string line;
-
-      while (getline(in, line) && line.find(sectname) != 0);
-      if (in.eof()) {
-        cerr << "EOF while looking for " << sectname << " section" << endl;
-        return false;
-      }
-      cerr << line << endl;
-      return true;
-    }
-    bool out_of_numeric_limits(id_t x) {
-      if (x >= numeric_limits<id_t>::max()) {
-        cerr << "Format id_t is too small to handle" << endl;
-        return true;
-      }
+namespace {
+  bool section_find(ifstream& in, const string& sectname, const fs::path& filename) {
+    string line;
+    while (getline(in, line) && line.find(sectname) != 0);
+    if (in.eof()) {
+      cerr << "EOF while looking for " << sectname << " section" << endl;
       return false;
     }
+    return true;
+  }
+  bool out_of_numeric_limits(id_t x) {
+    if (x >= numeric_limits<id_t>::max()) {
+      cerr << "Format id_t is too small to handle" << endl;
+      return true;
+    }
+    return false;
+  }
+}
+
+int io_file_manager(int argc, char** argv) {
+  if (--argc > 0)
+    input_path() = argv[1];
+  else {
+    cerr << "WARNING expected first console argument. ";
+    const string default_input_name = "input.txt";
+    cerr << "Replacing with " << default_input_name << " . . ." << endl;
+    input_path() = fs::path{ default_input_name };
+  }
+  if (--argc > 0)
+    output_path() = argv[2];
+  else {
+    cerr << "WARNING expected second console argument. ";
+    const string default_output_name = "output.txt";
+    cerr << "Replacing with " << default_output_name << " . . ." << endl;
+    output_path() = fs::path{ default_output_name }; 
+  }
+  if (--argc > 0) {
+    cerr << "WARNING too many arguments : last arguments in input ignored" << endl;
+  }
+  return 0;
+}
+fs::path& input_path() {
+  static fs::path res{};
+  return res;
+}
+fs::path& output_path() {
+  static fs::path res{};
+  return res;
+}
+
+int
+parse_map(
+  const fs::path& filepath,
+  sommet* const start,
+  sommet* const finish,
+  vector<obstacle>* const obstacles
+) {
+  //  Create input stream, constructor (5) from here:
+  //  https://en.cppreference.com/w/cpp/io/basic_ifstream/basic_ifstream
+  ifstream conf(filepath);
+
+  //Check file consistency
+  if (!conf.good()) {
+    cerr << "Cannot open map file" << endl;
+    return -1;
   }
 
-  int
-  read_map(
-      const path& filename,
-      sommet* start,
-      sommet* finish,
-      vector<obstacle>* obstacles
-    ) {
-    //Create input stream
-    ifstream conf(filename);
+  double x, y, r;
 
-    //Check file consistency
-    if (!conf.good()) {
-      cerr << "Cannot open map file" << endl;
+  //Parse $Start section
+
+  if (!section_find(conf, "[$Start]", filepath)) return -1;
+  conf >> x >> y >> r;
+  *start = sommet{ x,y };
+
+  //Parse $Finish section
+  if (!section_find(conf, "[$Finish]", filepath)) return -1;
+  conf >> x >> y;
+  *finish = sommet{ x,y };
+
+  //Parse $Obstacles section
+  if (!section_find(conf, "[$NumObstacles]", filepath)) return -1;
+  id_t num_obstacles;
+  conf >> num_obstacles;
+  if (out_of_numeric_limits(num_obstacles)) return -1;
+  obstacles->reserve(num_obstacles);
+
+  // Parse $Nodes section
+  if (!section_find(conf, "[$PtsObstacles]", filepath)) return -1;
+  for (id_t i = 0; i < num_obstacles; i++) {
+    obstacles->emplace_back(obstacle{});
+    auto* const vertices_of_i = &((*obstacles)[i].vertices);
+    id_t num_nodes_i;
+    conf >> num_nodes_i;
+    if (out_of_numeric_limits(num_nodes_i)) return -1;
+    if (num_nodes_i < 3) {
+      cerr << "Invalid number of nodes in obstacle " << i << endl;
       return -1;
     }
 
-    double x, y, r;
+    // here we introduce a simple algorithm how to parse a graph
 
-    string_type section;
+    // this lambda not only checks whether segments of construction are parallel
+    // but also parses to the obstacle object as many nodes of a construction as needed
+    auto segments_are_parallel = [&](vector<sommet> * const vertices,
+      sommet * const prev,
+      sommet * const curr,
+      const sommet & next) {
+      bool res = true;
 
-    //Parse $Start section
+      auto n1 = segment{ *prev, *curr }.outer_normal();
+      auto n2 = segment{ *curr,  next }.outer_normal();
 
-    if (!section_find(conf, "[$Start]", filename)) return -1;
-    conf >> x >> y;
-    *start = sommet{ x,y };
+      double sin_phi = det(n1, n2); // sin of angle between n1 and n2
 
-    //Parse $Finish section
-    if (!section_find(conf, "[$Finish]", filename)) return -1;
-    conf >> x >> y;
-    *finish = sommet{ x,y };
+      constexpr double tol = 1e-12;
+      if (abs(sin_phi) >= tol) {
+        // ok, segments are not parallel, thus, its intersection
+        // is significant
+        res = false;
+        // we consider 2 cases
+        if (sin_phi < 0.) {
+          // polygon obstacle_i is concave in vertex curr
+          // no smooth reconstruction of the 
+          // boundary needed
+          // we adjust boundary on radius r
+          double cos_phi = prod(n1, n2);
+          double k = sin_phi / (1. + cos_phi);
+          sommet inner_angle_shift = r * sommet{ n1.x() - k * n1.y(), k * n1.x() + n1.y() };
+          vertices->emplace_back(*curr + inner_angle_shift); // curr + shift = new angle point
+        }
+        else {
+          // polygon obstacle_i is convex in vertex curr
+          // compute smooth arc betwenn 2 successive shifts of curr
 
-    //Parse $Obstacles section
-    if (!section_find(conf, "[$NumObstacles]", filename)) return -1;
-    id_t num_obstacles;
-    conf >> num_obstacles;
-    if (out_of_numeric_limits(num_obstacles)) return -1;
-    obstacles->reserve(num_obstacles);
-
-    // Parse $Nodes section
-    if (!section_find(conf, "[$Nodes]", filename)) return -1;
-    for (id_t i = 0; i < num_obstacles; i++) {
-      obstacles->emplace_back(obstacle{});
-      auto& curr = (*obstacles)[i];
-      id_t num_nodes_i;
-      conf >> num_nodes_i;
-      if (out_of_numeric_limits(num_nodes_i)) return -1;
-      if (num_nodes_i < 3) {
-        cerr << "Invalid number of nodes in obstacle " << i << endl;
-        return -1;
+          if (r < tol)
+            vertices->emplace_back(*curr);
+          else {
+            constexpr double arc_0 = 1e-2; // relative length to consider
+            int num_parts = (int)(r*sin_phi/arc_0) + 1;
+            for (int k = 0; k <= num_parts; k++) {
+              double sin_phi_k = k * sin_phi / num_parts;
+              double cos_phi_k = sqrt(1. - sin_phi_k * sin_phi_k);
+              sommet outer_angle_shift = r * sommet{ cos_phi_k * n1.x() - sin_phi_k * n1.y(),
+                                                     sin_phi_k * n1.x() + cos_phi_k * n1.y() };
+              vertices->emplace_back(*curr + outer_angle_shift);
+            }
+          }
+        }
+        *prev = *curr; 
       }
-      curr.vertices.reserve(num_nodes_i);
-      for (id_t j = 0; j < num_nodes_i; j++) {
-        conf >> x >> y;
-        curr.vertices.emplace_back(sommet{ x,y });
-      }
+      *curr = next; // we do a step forward in our cycle
+      return res;
+    };
+
+    conf >> x >> y;
+    sommet prev{ x,y };  // read firse vertex
+    conf >> x >> y;
+    sommet curr{ x,y };  // read second vertex
+
+    sommet first = prev;
+    sommet second = curr; // memorize them in special variables
+                          // we will get back to them later
+
+    for (id_t j = 3; j <= num_nodes_i; j++) {
+      conf >> x >> y;
+      sommet next{ x,y }; // read next vertex
+      segments_are_parallel(vertices_of_i, &prev, &curr, next);
     }
-    conf.close();
-    return 0;
-  }
+    // but there are still two missing constructions to consider
+    segments_are_parallel(vertices_of_i, &prev, &curr, first);
+    if (segments_are_parallel(vertices_of_i, &prev, &curr, second))
+    // the last segment is parallel to the first one
+      (*vertices_of_i)[0] = prev;
+    // all the nodes of this obstacle processed
+  } // all obstacles processed
+  conf.close();
+  return 0;
 }
--- config.h
+++ config.h
@@ -1,21 +1,44 @@
 #include "obstacle.h"
 using namespace std;
 
-namespace config {
-	using path = filesystem::path;
-	using value_type = filesystem::path::value_type;
-	using string_type = filesystem::path::string_type;
+namespace {
+	template<typename T>
+	class has_begin_end {
+		using yes = byte;
+		using no = struct { yes dummy[2]; };
 
-	const path& input_file();
-	const path& output_file();
+		template<class U>
+		static auto test(U* u) -> decltype((*u).begin(), (*u).end(), yes());
 
-	template<class Container>
-	void output(const Container&);
-
-	int read_map(
-		const path& filename,
-		sommet* start,
-		sommet* finish,
-		vector<obstacle>* obstacles
-	);
+		static no   test(...);
+	public:
+		static constexpr bool value = (sizeof(test((T*)nullptr)) == sizeof(yes));
+	};
 }
+
+namespace fs = filesystem;
+
+template<typename ptrs_holder>
+inline
+typename enable_if<has_begin_end<ptrs_holder>::value>::type
+write_ptrs(const ptrs_holder& cont) {
+	fs::copy_file(input_path(), output_path(), fs::copy_options::overwrite_existing);
+	ofstream out(output_path(), ios::app);
+	out << endl;
+	out << "[$OptPath]" << endl;
+	for (const auto& elem : cont) {
+		out << elem->x() << ' ' << elem->y() << endl;
+	}
+	out.close();
+}
+
+int io_file_manager(int, char**);
+fs::path& input_path();
+fs::path& output_path();
+
+int parse_map(
+	const fs::path& filename,
+	sommet* const start,
+	sommet* const finish,
+	vector<obstacle>* const obstacles
+);
