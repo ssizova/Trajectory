46b67a31369d41673b083642277f64fd71e027ff
NikitaMatckevich <57538710+NikitaMatckevich@users.noreply.github.com>
2020-04-02 20:33:52 +0200

    fixed spaces

--- obstacle.cpp
+++ obstacle.cpp
@@ -3,7 +3,7 @@
 segment obstacle::segments(size_t i) const {
   auto n = vertices.size();
   assert(i < n);
-	return segment{vertices[i], vertices[(i + 1) % n]};
+  return segment{ vertices[i], vertices[(i + 1) % n] };
 }
 bool obstacle::contains_inside(const sommet& p) const {
   int counter = 0; // the  winding number counter
@@ -11,11 +11,11 @@ bool obstacle::contains_inside(const sommet& p) const {
   for (size_t i = 0; i < n; i++) {
     const sommet& beg = vertices[i];
     const sommet& end = vertices[(i + 1) % n];
-    if (beg.y() <= p.y()) { 
-      if ((end.y() >  p.y()) && (det(end - beg, p - beg) > 0.)) // p left of edge
+    if (beg.y() <= p.y()) {
+      if ((end.y() > p.y()) && (det(end - beg, p - beg) > 0.)) // p left of edge
         ++counter; // a valid up intersect
     }
-    else {                      
+    else {
       if ((end.y() <= p.y()) && (det(end - beg, p - beg) < 0.)) // p right of edge
         --counter; // a valid down intersect
     }
